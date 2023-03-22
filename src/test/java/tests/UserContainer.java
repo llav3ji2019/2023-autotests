c1e3a3dc3e5f3f3277961ad9f14e258c6188cf4b
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-03-22 08:43:03 +0300

    adding unique data for tests(Data registry)

---
+++ src/test/java/tests/UserContainer.java
@@ -0,0 +1,36 @@
+package tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import user.User;
+import user.UserHandler;
+
+public class UserContainer {
+    private final List<User> users = new ArrayList<>();
+    private static int counter;
+
+    public UserContainer() {
+        users.add(new UserHandler()
+                .setLogin("botS23AT9")
+                .setName("botS23AT9 botS23AT9")
+                .setPassword("autotests2023")
+                .build());
+
+        users.add(new UserHandler()
+                .setLogin("botS23AT28")
+                .setName("botS23AT28 botS23AT289")
+                .setPassword("autotests2023")
+                .build());
+
+        users.add(new UserHandler()
+                .setLogin("botS23AT27")
+                .setName("botS23AT27 botS23AT27")
+                .setPassword("autotests2023")
+                .build());
+    }
+
+    public User getUniqueUser() {
+        return users.get(counter++);
+    }
+}
