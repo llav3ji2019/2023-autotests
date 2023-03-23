27ce33f8052d9cb832b02094bc4e631dd55f3d66
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-03-23 11:53:26 +0300

    fix MessageTest

--- src/test/java/page/friends/ChatPage.java
+++ src/test/java/page/friends/ChatPage.java
@@ -7,14 +7,18 @@ import com.codeborne.selenide.SelenideElement;
 
 import page.LoadableComponent;
 
+import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ChatPage implements LoadableComponent {
     private static final long TIME_OUT_IN_SECONDS = 5;
     private static final SelenideElement CHAT_TITLE = $x("//span[@data-tsid='chat_title']");
     private static final SelenideElement TEXT_INPUT = $x("//div[@data-tsid='write_msg_input-input']");
+    private static final SelenideElement LAST_MESSAGE_SENT = $$x("//*[@data-tsid='message_text']").last();
     private static final String CHAT_ITEM_MESSAGE = "It is not needed user";
     private static final String TEXT_INPUT_MESSAGE = "Can't find input where i can enter some text";
+    private static final String LAST_SENT_MESSAGE = "Can't find message";
 
     public ChatPage check() {
         isLoaded(CHAT_TITLE, CHAT_ITEM_MESSAGE, TIME_OUT_IN_SECONDS);
@@ -26,8 +30,12 @@ public class ChatPage implements LoadableComponent {
         return isLoaded(CHAT_TITLE, CHAT_ITEM_MESSAGE, TIME_OUT_IN_SECONDS).text();
     }
 
-    public ChatPage sendMessage(@NotNull final String text) {
-        isLoaded(TEXT_INPUT, TEXT_INPUT_MESSAGE, TIME_OUT_IN_SECONDS).setValue(text).sendKeys(Keys.ENTER);
+    public String getLastMessageText() {
+        return isLoaded(LAST_MESSAGE_SENT, LAST_SENT_MESSAGE, TIME_OUT_IN_SECONDS).text();
+    }
+
+    public ChatPage sendMessage(@NotNull final String myMessage) {
+        isLoaded(TEXT_INPUT, TEXT_INPUT_MESSAGE, TIME_OUT_IN_SECONDS).setValue(myMessage).sendKeys(Keys.ENTER);
         return this;
     }
 }
--- src/test/java/tests/messages/MessageTest.java
+++ src/test/java/tests/messages/MessageTest.java
@@ -15,8 +15,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MessageTest extends BaseTest {
     private static final String FRIEND_NAME = "Павел Емельянов";
-    private static final String DEFAULT_TEXT = "Hello world!";
+    private static final String DEFAULT_TEXT = "Hello world!11";
     private static final String WRONG_CHAT_TITLE = "We are trying to send message to wrong friend";
+    private static final String LAST_MESSAGE_SENT_MESSAGE = "Message wasn't sent";
 
     private HomePage homePage;
     private final User user = new UserContainer().getUniqueUser();
@@ -32,5 +33,7 @@ public class MessageTest extends BaseTest {
         ChatPage chatPage = homePage.openFriendPage().check().openChat(FRIEND_NAME);
         assertThat(WRONG_CHAT_TITLE, chatPage.getChatTitle().equals(FRIEND_NAME));
         chatPage.sendMessage(DEFAULT_TEXT);
+        assertThat(LAST_MESSAGE_SENT_MESSAGE, DEFAULT_TEXT.equals(chatPage.getLastMessageText()));
+
     }
 }
