615abc84b21ad59b1c399c81bff807e7c4fb74da
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-03-24 02:36:45 +0300

    fix all tests

--- src/test/java/tests/AllTests.java
+++ src/test/java/tests/AllTests.java
@@ -4,11 +4,13 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 import tests.callfriend.CallFriendTest;
+import tests.deletegroup.DeleteAllMyGroups;
+import tests.joingroup.JoinGroupTest;
 import tests.login.LoginTest;
 import tests.logout.LogOutTest;
 import tests.messages.MessageTest;
 
 @Suite
-@SelectClasses({MessageTest.class, LoginTest.class, LogOutTest.class, CallFriendTest.class})
+@SelectClasses({MessageTest.class, LoginTest.class, JoinGroupTest.class, CallFriendTest.class, LogOutTest.class, DeleteAllMyGroups.class})
 public class AllTests {
 }
